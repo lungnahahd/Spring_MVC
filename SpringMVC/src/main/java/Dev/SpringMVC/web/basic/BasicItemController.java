5452408ca62b928e661cb77467edfc84cb32b586
lungnahahd <jyoungjk33@gmail.com>
2024-02-18 01:48:12 +0900

    Add : item.html 상품 상세 페이지 추가

--- SpringMVC/src/main/java/Dev/SpringMVC/web/basic/BasicItemController.java
+++ SpringMVC/src/main/java/Dev/SpringMVC/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import Dev.SpringMVC.domain.item.Item;
@@ -33,6 +34,16 @@ public class BasicItemController
 		return "basic/items";
 	}
 	
+	@GetMapping("/{itemId}")
+	public String itemDetail(@PathVariable(name = "itemId") long itemId, Model model) 
+	{
+		Item nowFindItem = itemRepository.findById(itemId);
+		model.addAttribute("item", nowFindItem);
+		return "basic/item";
+	}
+	
+	
+	// 테스트 데이터 생성 
 	@PostConstruct
 	public void init() 
 	{
