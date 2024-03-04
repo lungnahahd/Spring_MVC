646e4316b9cacb6290e55d8590e8187651cf48d1
lungnahahd <jyoungjk33@gmail.com>
2024-03-05 00:34:09 +0900

    Add : addForm() 상품 등록 폼 생성 및 관련 함수 맵핑

--- SpringMVC/src/main/java/Dev/SpringMVC/web/basic/BasicItemController.java
+++ SpringMVC/src/main/java/Dev/SpringMVC/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import Dev.SpringMVC.domain.item.Item;
@@ -42,6 +43,17 @@ public class BasicItemController
 		return "basic/item";
 	}
 	
+	@GetMapping("/add")
+	public String addForm() 
+	{
+		return "basic/addForm";
+	}
+	
+	@PostMapping("/add")
+	public String save() 
+	{
+		return "basic/addForm";
+	}
 	
 	// 테스트 데이터 생성 
 	@PostConstruct
