a8fb625bc931db2d8b0b72f2bfe8b24c96b77717
lungnahahd <jyoungjk33@gmail.com>
2024-03-17 01:30:59 +0900

    Fix : 상품 등록 처리 수정 @ModelAttribute 사용으로 수정

--- SpringMVC/src/main/java/Dev/SpringMVC/web/basic/BasicItemController.java
+++ SpringMVC/src/main/java/Dev/SpringMVC/web/basic/BasicItemController.java
@@ -1,15 +1,16 @@
 package Dev.SpringMVC.web.basic;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import Dev.SpringMVC.domain.item.Item;
 import Dev.SpringMVC.domain.item.ItemRepository;
@@ -51,18 +52,9 @@ public class BasicItemController
 	}
 	
 	@PostMapping("/add")
-	public String save(@RequestParam(value="itemName") String itemName,
-					   @RequestParam(value="price") Integer price,
-					   @RequestParam(value="quantity") Integer quantity,
-					   Model model) 
+	public String save(@ModelAttribute("item") Item item, Model model) 
 	{
-		Item item = new Item();
-		item.setItemName(itemName);
-		item.setPrice(price);
-		item.setQuentity(quantity);
-		
 		itemRepository.save(item);
-		
 		model.addAttribute("item", item);
 		
 		return "basic/item";
